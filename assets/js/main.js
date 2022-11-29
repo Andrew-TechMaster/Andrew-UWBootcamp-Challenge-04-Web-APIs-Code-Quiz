bfa13957dca476ed088e566651f18e7e3729016d
Andrew Chou <andrewchou1996@gmail.com>
2022-11-29 14:58:37 -0800

    Add methods: getQuestion & endGame

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 /* {============================= DOM / Element / Variable Declaration  =============================} */
 /* <------ SectionEl ------> */
-var menuEl = document.querySelector("#menuSection");
-var quizEl = document.querySelector("#quizSection");
-var settleEl = document.querySelector("#settleSection");
-var viewhighScoreEl = document.querySelector("#viewScoreSection");
+var menuSectionEl = document.querySelector("#menuSection");
+var quizSectionEl = document.querySelector("#quizSection");
+var settleSectionEl = document.querySelector("#settleSection");
+var viewhighScoreSectionEl = document.querySelector("#viewScoreSection");
 /* <------ ButtonEl ------> */
 var viewBtnEl = document.querySelector("#viewBtn");
 var startBtnEl = document.querySelector("#startBtn");
@@ -39,10 +39,34 @@ function countDown() {
             // Stops execution of action at set interval
             clearInterval(timeInterval);
             timerEl.textContent = "is up";
+            endGame();
         }
     }, 1000)
 }
 
+/* <------ Get Quesion / Next Question ------> */
+function getQuestion() {
+    menuSectionEl.className = "hidden";
+    quizSectionEl.className = "visible";
+
+    if (questionIndex < lengthOfQuestions) {
+        questionTitleEl.textContent = setOfQuestions[questionIndex].title;
+        optionAEl.textContent = setOfQuestions[questionIndex].choices[0];
+        optionBEl.textContent = setOfQuestions[questionIndex].choices[1];
+        optionCEl.textContent = setOfQuestions[questionIndex].choices[2];
+        optionDEl.textContent = setOfQuestions[questionIndex].choices[3];
+    } else {
+        endGame();
+    }
+}
+
+/* <------ End Game / Game Over ------> */
+function endGame() {
+    quizSectionEl.className = "hidden";
+    settleSectionEl.className = "visible";
+
+    displayScoreEl.textContent = `Your Final Score is ${totalScore}`
+}
 /* {============================= Add Event Listener  =============================} */
 // btn.addEventListener("click", callback)
 
@@ -60,8 +84,10 @@ function countDown() {
 // clearAll() => clear local storage
 console.log(setOfQuestions);
 console.log(`#Question: ${lengthOfQuestions} & Total Time: ${timeLeft} `);
+console.log(setOfQuestions[questionIndex].choices[3])
 
 startBtnEl.addEventListener("click", function (evt) {
     evt.preventDefault();
-    countDown();
+    // countDown();
+    getQuestion();
 });
