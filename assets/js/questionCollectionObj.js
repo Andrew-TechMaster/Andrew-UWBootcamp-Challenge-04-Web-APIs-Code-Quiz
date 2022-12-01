cba7ba976aa4d9df430e9d735f7bc89706499ceb
Andrew Chou <andrewchou1996@gmail.com>
2022-11-30 16:51:17 -0800

    Add 5 more questions in this project

--- assets/js/questionCollectionObj.js
+++ assets/js/questionCollectionObj.js
@@ -1,17 +1,32 @@
- const questionCollection = [
+const questionCollection = [
     {
         title: "Commonly used data types DO NOT include: ",
         choices: ["strig", "booleans", "alert", "numbers"],
         answer: "alert"
     },
     {
-        title: "Question 02",
-        choices: ["c1", "c2", "c3", "c4"],
-        answer: "c2"
+        title: "What methods can be used on HTML elements returns and will return true if an element matches a specific CSS selector(s).",
+        choices: ["contains", "includes", "matches", "containsKey"],
+        answer: "matches"
     },
     {
-        title: "Question 3",
-        choices: ["c1", "c2", "c3", "c4"],
-        answer: "c3"
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<scripting>", "<js>", "<javascript>"],
+        answer: "script"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["<body>", "<head>", "both", "none of the above"],
+        answer: "both"
+    },
+    {
+        title: "What does 'isNaN' mean in JavaScript?",
+        choices: ["Not A Number", "Not A Name", "Not A Neighbor", "Not A Nature"],
+        answer: "Not A Number"
+    },
+    {
+        title: "What is the author name of this application",
+        choices: ["Andrew", "Andre", "Andreson", "Andy"],
+        answer: "Andrew"
     }
 ];
